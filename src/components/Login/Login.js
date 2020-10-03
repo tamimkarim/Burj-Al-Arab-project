c600898c7a7ff1a43aedee4caf41690a8a855cd7
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-10-04 05:20:14 +0600

    user info

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import {UserContext} from '../../App';
 
 const Login = () => {
-    firebase.initializeApp(firebaseConfig);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    if(firebase.apps.length ===0){
+        firebase.initializeApp(firebaseConfig);
+    }
+
     const handleGoogleSignIn = () =>{
         const provider = new firebase.auth.GoogleAuthProvider();
 
         firebase.auth().signInWithPopup(provider).then(function(result) {
-            var token = result.credential.accessToken;
-            var user = result.user;
-            console.log(user);
+            const {displayName, email} = result.user;
+            const signedInUser = {name: displayName, email}
+            setLoggedInUser(signedInUser);
+            console.log(signedInUser);
 
           }).catch(function(error) {
-            // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
-            // The email of the user's account used.
             var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            // ...
           });
 
     }
